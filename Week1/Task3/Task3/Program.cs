c9a7b8a9abb26bc7b97ab85c668104cea10ec492
Nurbergen Khinatolla <nhinatolla@gmail.com>
2019-01-28 00:05:47 +0600

    Week1 Task3

---
+++ Week1/Task3/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        private static void dublicate(string[] s, int n)
+        {
+            for (int i = 0; i < n; ++i)
+                Console.Write(s[i] + " " + s[i] + " ");
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()); // reading first line and converting to int
+            string[] reading = Console.ReadLine().Split(); // reading second line an array of string by split
+            dublicate(reading, n); // Method thats dubplicate each of element
+        }
+    }
+}
