50a8777ccb5594a7c0e41719f31a3ad4fcbe860d
Nurbergen Khinatolla <nhinatolla@gmail.com>
2019-01-28 00:23:15 +0600

    Week1 Task4

---
+++ Week1/Task4/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; ++i, Console.WriteLine()) // Skills from C++, nothing else
+                for (int j = 1; j <= i; ++j)
+                    Console.Write("[*]");
+        }
+    }
+}
