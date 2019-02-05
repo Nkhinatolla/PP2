a171098cc36c5fcfb5477ad2d6108982a8f32daa
Nurbergen Khinatolla <nhinatolla@gmail.com>
2019-02-05 22:11:04 +0600

    Week1 Task4: Using 2d string array

--- Week1/Task4/Task4/Program.cs
+++ Week1/Task4/Task4/Program.cs
@@ -11,9 +11,13 @@ namespace Task4
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i <= n; ++i, Console.WriteLine()) // Skills from C++, nothing else
-                for (int j = 1; j <= i; ++j)
-                    Console.Write("[*]");
+            string[,] d = new string[n, n];
+            for (int i = 0; i < n; ++i)
+                for (int j = 0; j <= i; ++j)
+                    d[i, j] = "[*]";
+            for (int i = 0; i < n; ++i, Console.WriteLine())
+                for (int j = 0; j <= i; ++j)
+                    Console.Write(d[i, j]);
         }
     }
 }
