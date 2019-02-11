7f32cb458861921a51bcce5c336142dad91d3495
Nurbergen Khinatolla <nhinatolla@gmail.com>
2019-02-11 15:42:01 +0600

    Upd: Week1 Task2

--- Week1/Task2/Task2/Student.cs
+++ Week1/Task2/Task2/Student.cs
@@ -9,7 +9,7 @@ public class Student
     {
         this.name = name;
         this.id = id;
-        this.year = year + 1;
+        this.year = year;
     }
     public Student()
     {
@@ -20,11 +20,11 @@ public class Student
         }
         this.name = reading[0];
         this.id = Convert.ToInt32(reading[1]);
-        this.year = Convert.ToInt32(reading[2]) + 1;
+        this.year = Convert.ToInt32(reading[2]);
     }
     public void access()
     {
-        Console.WriteLine(name + " " + id + " " + year);
+        Console.WriteLine(name + " " + id + " " + (year + 1));
     }
 
 }
