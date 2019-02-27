37619eecaba8b5fd3770c28ad8f2d9718c2623c3
Nurbergen Khinatolla <nhinatolla@gmail.com>
2019-02-28 00:59:38 +0600

    UPD: Task2

--- Week5/Task1/Task1/Program.cs
+++ Week5/Task1/Task1/Program.cs
@@ -10,69 +10,61 @@ namespace Task1
     [Serializable]
     public class ComplexNumber
     {
-        public double a, b;
-        public ComplexNumber()
-        {
-
-        }
-        public ComplexNumber(double _re, double _im)
-        {
-            a = _re;
-            b = _im;
-        }
+        public double a { get; set;}
+        public double b { get; set;}
         public static ComplexNumber operator +(ComplexNumber num1, ComplexNumber num2)
         {
-            return new ComplexNumber(num1.a + num2.a, num1.b + num2.b);
+            return new ComplexNumber { a = num1.a + num2.a, b = num1.b + num2.b};
         }
         public static ComplexNumber operator -(ComplexNumber num1, ComplexNumber num2)
         {
-            return new ComplexNumber(num1.a - num2.a, num1.b - num2.b);
+            return new ComplexNumber { a = num1.a - num2.a, b = num1.b - num2.b};
         }
         public static ComplexNumber operator *(ComplexNumber num1, ComplexNumber num2)
         {
-            return new ComplexNumber((num1.a * num2.a) - (num1.b * num2.b), (num1.b * num2.a) + (num2.b * num1.a));
+            return new ComplexNumber { a = (num1.a * num2.a) - (num1.b * num2.b), b = (num1.b * num2.a) + (num2.b * num1.a) };
         }
         public static ComplexNumber operator /(ComplexNumber num1, ComplexNumber num2)
         {
             double D = (num2.a * num2.a + (num2.b * num2.b));
-            return new ComplexNumber((num1.a * num2.a + num1.b * num2.b) / D, -(num1.a * num2.b - num1.b * num2.a) / D);
+            return new ComplexNumber { a = (num1.a * num2.a + num1.b * num2.b) / D, b = -(num1.a * num2.b - num1.b * num2.a) / D };
         }
-        public void info()
+        public void output()
         {
-            Console.Write(a.ToString());
+            if (a != 0)
+                Console.Write(a.ToString());
             if (b == 0)
                 return;
-            if (b < 0)
-                Console.Write('-');
-            else
+            if (b > 0 && a != 0)
                 Console.Write('+');
-            Console.WriteLine(Math.Abs(b).ToString() + 'i');
+            Console.WriteLine(b.ToString() + 'i');
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            ComplexNumber A = new ComplexNumber(5, 4);
-            ComplexNumber B = new ComplexNumber(8, 3);
-            ComplexNumber C = (A * B);
-            C.info();
-            //Ser(C);
-            ComplexNumber D = Deser();
-            D.info();
+            ComplexNumber A = new ComplexNumber { a = 5, b = 4};
+            ComplexNumber B = new ComplexNumber { a = 5, b = 5};
+            ComplexNumber C = (A - B) * B;
+            C.output();
+            Deser();
+            Ser(C);
         }
         static void Ser(ComplexNumber c) 
         {
-            FileStream fs = new FileStream("ComplexNumber", FileMode.Create, FileAccess.Write);
+            FileStream fs = new FileStream("ComplexNumber.txt", FileMode.Create, FileAccess.Write);
             XmlSerializer xs = new XmlSerializer(typeof(ComplexNumber));
             xs.Serialize(fs, c);
             fs.Close();
         }
-        static ComplexNumber Deser()
+        static void Deser()
         {
-            FileStream fs = new FileStream("ComplexNumber", FileMode.Open, FileAccess.Read);
+            FileStream fs = new FileStream("ComplexNumber.txt", FileMode.Open, FileAccess.Read);
             XmlSerializer xs = new XmlSerializer(typeof(ComplexNumber));
-            return xs.Deserialize(fs) as ComplexNumber;
+            ComplexNumber t = xs.Deserialize(fs) as ComplexNumber;
+            t.output();
+            fs.Close();
 
         }
     }
