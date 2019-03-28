ece8d80d5d967e7a7b0f82de48b4a9fffc3ee580
TeamWorkDevelopers <TeamWorkDevelopers4@gmail.com>
2019-03-28 22:31:08 +0600

    Lab6: Threads

---
+++ Week7(2nd)/Task1/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Task1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Thread[] threadArray = new Thread[3];
+            for (int i = 0; i < 3; i++)
+            {
+                threadArray[i] = new Thread(DoIt);
+                threadArray[i].Name = "Marvel" + i;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                threadArray[i].Start();
+            }
+
+        }
+        static void DoIt()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+            }
+        }
+
+    }
+}
