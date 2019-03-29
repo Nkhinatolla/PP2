3b4ce68b0eff88041f890d24939f0752c76b1855
TeamWorkDevelopers <TeamWorkDevelopers4@gmail.com>
2019-03-30 01:49:52 +0600

    ForKami

---
+++ Quiz/Task1/Task1/Program (2).cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Threads
+{
+    class Program
+    {
+        static List<ConsoleColor> l = new List<ConsoleColor> { ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Green };
+        static Timer timer = new Timer(1000);
+        static Timer timer2 = new Timer();
+        static void Main(string[] args)
+        {
+            timer.Elapsed += Timer_Elapsed1;
+            timer.Start();
+            timer2.Elapsed += Timer2_Elapsed;
+            ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+            while (true) {
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.Enter:
+                        timer2.Start();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        private static void Timer2_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.SetCursorPosition(0, 1);
+            timer.Enabled = false;
+            Console.Write("Input word with length>8:");
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+            timer.Enabled = true;
+            Environment.Exit(0);
+        }
+
+        private static void Timer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.Write(DateTime.Now);
+        }
+
+
+    }
+}
---
+++ Quiz/Task1/Task1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task1
+{
+    public class Employee
+    {
+        private string name;
+        private string id;
+        private int salary;
+        public string N
+        {
+            get {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string I
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public int S {
+            get
+            {
+                return salary += 50000;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee x = new Employee { N = "Alex", I = "18BD", S = 10000};
+            int t = x.S + x.S;
+            Ser(x);
+            Deser(x);
+        }
+        static void Ser(Employee c)
+        {
+            FileStream fs = new FileStream("Employee.xml", FileMode.Create, FileAccess.Write);
+            XmlSerializer xs = new XmlSerializer(typeof(Employee));
+            xs.Serialize(fs, c);
+            fs.Close();
+        }
+        static Employee Deser(Employee c)
+        {
+            FileStream fs = new FileStream("Employee.xml", FileMode.Open, FileAccess.Read);
+            XmlSerializer xs = new XmlSerializer(typeof(Employee));
+            Employee x = xs.Deserialize(fs) as Employee;
+            fs.Close();
+            return x;
+        }
+    }
+}
